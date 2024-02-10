3709b8934ed7a55affc95308a5f210daf402dc92
iamASR27 <ramardeep1997@gmail.com>
2024-02-10 18:48:39 +0530

    Serving HTML , CSS and JS (VIEWS)

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const contactUsRoutes = require('./routes/contact-us');
+const successRoutes = require('./routes/success');
 
 // app.use((req, res, next) => {
 //     console.log("In the middleware");
@@ -21,11 +24,16 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.use('/admin', adminRoutes);
-app.use('/shop', shopRoutes);
+app.use(shopRoutes);
+app.use(contactUsRoutes);
+app.use(successRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found</h1>');
+    // res.status(404).send('<h1>Page Not Found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404Error.html'));
 })
 
 // const server = http.createServer(app);
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const path = require('path');
+
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 // router.get('/add-product', (req, res, next) => {
@@ -8,13 +12,14 @@ const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title" /><input type="number" name="size" /><button type="submit">Add Product</button></form>');
+    // res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 })
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
     console.log(req.body);
-    res.redirect('/shop');
+    res.redirect('/');
 })
 
 // router.post('/product', (req, res, next) => {
